dac2aea7d376aec275920390db2328f68f8ba683
xym <xuyiminxuyimin@126.com>
2017-07-27 06:00:21 +0800

    user.balance功能修正，此前返回的是第1条持仓数据。

--- easytrader/yh_clienttrader.py
+++ easytrader/yh_clienttrader.py
@@ -191,12 +191,13 @@ class YHClientTrader():
         win32api.PostMessage(tree_view_hwnd, win32con.WM_KEYDOWN, win32con.VK_F4, 0)
         time.sleep(0.5)
         self.capital_window_hwnd = win32gui.GetDlgItem(operate_frame_hwnd, 0xE901)  # 资金股票窗口框架
+        self.capital_grid = win32gui.GetDlgItem(self.capital_window_hwnd, 0x51C) 
 
     def balance(self):
         return self.get_balance()
 
     def get_balance(self):
-        self._set_foreground_window(self.capital_window_hwnd)
+        self._set_foreground_window(self.capital_grid)
         time.sleep(0.3)
         data = self._read_clipboard()
         return self.project_copy_data(data)[0]
